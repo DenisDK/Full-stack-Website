a96478fb227ab5eb7320fdc947d222581169a7c7
Kushniruk Denis <denys.kushniruk@nure.ua>
2024-05-07 17:55:14 +0300

    Project layout

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Links } from "./links/Links";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <div className="">Logo</div>
+      <div className="">
+        <Links></Links>
+      </div>
+    </div>
+  );
+};
