b6463f98d0462b435a449c0ed7ecd2e3057e0d82
Kushniruk Denis <denys.kushniruk@nure.ua>
2024-05-07 21:36:50 +0300

    Create footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>DenisDk</div>
+      <div className={styles.text}>
+        DenisDk creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 };
