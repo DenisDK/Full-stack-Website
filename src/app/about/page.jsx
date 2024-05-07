90ca890ef3fe8d66f4f60128902924b6d9c923ec
Kushniruk Denis <denys.kushniruk@nure.ua>
2024-05-07 21:33:00 +0300

    Create Home page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./about.module.css";
 
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/about.png"
+          alt="about img"
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          priority={true} // {false} | {true}
+        />
+      </div>
+    </div>
+  );
 }
