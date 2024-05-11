515ca0b69ab6b302b16215f9a340ef71f9dceba0
Kushniruk Denis <denys.kushniruk@nure.ua>
2024-05-11 17:05:05 +0300

    Create Middleware

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,30 +3,26 @@ import GitHub from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDb } from "./utils";
 import { User } from "./models";
-import credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
+import { authConfig } from "./auth.config";
 
 const login = async (credentials) => {
   try {
     connectToDb();
     const user = await User.findOne({ username: credentials.username });
 
-    if (!user) {
-      throw new Error("Wrong credentials!");
-    }
+    if (!user) throw new Error("Wrong credentials!");
 
     const isPasswordCorrect = await bcrypt.compare(
       credentials.password,
       user.password
     );
 
-    if (!isPasswordCorrect) {
-      throw new Error("Wrong credentials!");
-    }
+    if (!isPasswordCorrect) throw new Error("Wrong credentials!");
 
     return user;
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
     throw new Error("Failed to login!");
   }
 };
@@ -37,6 +33,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  ...authConfig,
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID,
@@ -47,7 +44,7 @@ export const {
         try {
           const user = await login(credentials);
           return user;
-        } catch (error) {
+        } catch (err) {
           return null;
         }
       },
@@ -55,7 +52,6 @@ export const {
   ],
   callbacks: {
     async signIn({ user, account, profile }) {
-      // console.log(user, account, profile);
       if (account.provider === "github") {
         connectToDb();
         try {
@@ -67,14 +63,16 @@ export const {
               email: profile.email,
               image: profile.avatar_url,
             });
+
             await newUser.save();
           }
-        } catch (error) {
-          console.log(error);
+        } catch (err) {
+          console.log(err);
           return false;
         }
       }
       return true;
     },
+    ...authConfig.callbacks,
   },
 });
